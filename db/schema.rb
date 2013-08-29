2f8d97a6a1c9774bcb5c8816bfaa41a8ed2c8e32
David Patnode <draknor@gmail.com>
2013-08-29 14:10:22 -0500

    Add db config + updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130829134752) do
+ActiveRecord::Schema.define(:version => 20130829144455) do
 
   create_table "data", :force => true do |t|
     t.integer  "user_id"
     t.integer  "datumType_id"
-    t.float    "value"
+    t.string   "value"
     t.string   "comment"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
