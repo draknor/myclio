369173f1761e769ade3f357d84c5851fc6795b1b
David Patnode <draknor@gmail.com>
2013-08-29 05:44:40 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myclio::Application.config.secret_token = '7014360f025dfeee1ae5596d6f7ecf6133a6dd6147723dc3a68e1dd9329aabdf5d2042c6022979ba7aa52f41bfbfc3cdcb8796dd1bce7549d68d472fe754727f'
